4277085533c0634cb52f7133639ec16a43b36e9c
Dungdz126 <phmchidung852@gmail.com>
2023-09-29 16:37:36 +0700

    update so nguyen to

--- so_nguyen_to.cpp
+++ so_nguyen_to.cpp
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
 int main(){
     int n,c=0;
     cin >>n;
-    if(n<2){
-        cout<<"Day Ko Phai So Nguyen To Xin Nhap Lai"<<"\n";
-        cin>>n;
-    }
-    for(int i=2;i<=sqrt(n);i++){
-        if(n%i==0)
-            c++;
-    }
-    if(c==0){
-        cout<<"n la so nguyen to";
+    if (n<2){
+        cout<<"0";
     }else{
-        cout<<"n ko phai la so nguyen to";
+    for (int i=2;i<=sqrt(n);i++){
+        if(n%i==0){
+            c++;
+        }
     }
+        if(c==0){
+            cout<<"1";
+            exit(0);
+        }        
+    }
+    cout<<"0";
     return 0;
 }
