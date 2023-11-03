0365a2c4f4f8e8e0ff911294459185be3aecb5d9
DUNG KO BIET GI HET <123146672+Dungkobietcode@users.noreply.github.com>
2023-11-03 14:32:01 +0700

    cau1

---
+++ CAU1.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    freopen("CAU01.INP","r",stdin);
+    freopen("CAU01.OUT","w",stdout);
+    int n;
+    cin>>n;
+    if(n<0){
+        cout<<"Invalid";
+    }else if(n%400==0 || n%4==0 && n%100!=0){
+        cout<<"Yes";19
+    }else{
+        cout<<"No";
+    }
+    return 0;
+}
